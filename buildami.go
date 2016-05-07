6a291ec54f2bad8e7d67cc1b810c81aaed0f58de
Jeremy Deininger <jeremydeininger@gmail.com>
2016-05-07 12:57:03 -0700

    first commit; all builds are pass

---
+++ buildami.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	//"gopkg.in/hypersleep/easyssh.v0"
+	"github.com/jeremyd/easyssh"
+	"time"
+	"bufio"
+	"os"
+	"flag"
+	"io/ioutil"
+)
+
+func req_spot(client *ec2.EC2, ami_id string, price string) (*string) {
+	sec_group_ids := make([]*string, 1)
+	sec0 := "sg-8671c2b5"
+	sec_group_ids[0] = &sec0
+
+	keyname := "newcarl"
+	instancetype := ec2.InstanceTypeM3Large
+
+	lspec := ec2.RequestSpotLaunchSpecification{
+		KeyName: &keyname,
+		//UserData: ""54.184.34.9,
+		ImageId: &ami_id,
+		InstanceType: &instancetype,
+		SecurityGroupIds: sec_group_ids }
+
+	params := ec2.RequestSpotInstancesInput{
+		SpotPrice: &price,
+		LaunchSpecification: &lspec }
+
+	req, resp := client.RequestSpotInstancesRequest(&params)
+	err := req.Send()
+	if err == nil { // resp is now filled
+		//fmt.Println(resp)
+	}
+	return resp.SpotInstanceRequests[0].SpotInstanceRequestId
+}
+
+func wait_for_spot_req_active(client *ec2.EC2, spot_req_id *string) (instance_id *string) {
+	params := &ec2.DescribeSpotInstanceRequestsInput{
+		SpotInstanceRequestIds: []*string{ spot_req_id } }
+
+	for {
+		resp, err := client.DescribeSpotInstanceRequests(params)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		if *resp.SpotInstanceRequests[0].State == ec2.SpotInstanceStateActive {
+			//fmt.Println(resp)
+			return resp.SpotInstanceRequests[0].InstanceId
+		}
+		time.Sleep(5 * time.Second)
+		//fmt.Println(resp)
+	}
+}
+
+func get_instance_ip(client *ec2.EC2, id *string) (ip *string) {
+	params := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{ id } }
+
+	resp, err := client.DescribeInstances(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//fmt.Println(resp)
+	return resp.Reservations[0].Instances[0].PublicIpAddress
+}
+
+func write_spot_state(spot_req_id *string) {
+		d1 := []byte(*spot_req_id)
+    err := ioutil.WriteFile(".hello.state", d1, 0644)
+		if err != nil {
+			fmt.Println("WARNING: error occured writing to .hello.state file")
+		}
+}
+
+func create_and_attach_volume(client *ec2.EC2, instance_id *string) (volume_id *string) {
+	// DESCRIBE INSTANCE TO GET METADATA
+	iparams := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{ instance_id } }
+	iresp, ierr := client.DescribeInstances(iparams)
+	if ierr != nil {
+		fmt.Println(ierr.Error())
+		return
+	}
+	//fmt.Println(iresp)
+	availability_zone := iresp.Reservations[0].Instances[0].Placement.AvailabilityZone
+
+	// CREATE VOLUME
+	vparams := &ec2.CreateVolumeInput{
+		AvailabilityZone: availability_zone, // Required
+		Size:             aws.Int64(10),
+		VolumeType:       aws.String("gp2"),
+	}
+	vresp, verr := client.CreateVolume(vparams)
+	if verr != nil {
+		fmt.Println(verr.Error())
+		return
+	}
+	//fmt.Println(vresp)
+	save_volume_id := vresp.VolumeId
+
+	// ATTACH VOLUME TO INSTANCE
+	keep_waiting_attach := true
+	for keep_waiting_attach {
+		avparams := &ec2.AttachVolumeInput{
+			Device:     aws.String("/dev/sdx"),
+			InstanceId: instance_id,
+			VolumeId:   save_volume_id }
+		_, averr := client.AttachVolume(avparams)
+		if averr != nil {
+			fmt.Println(averr.Error())
+			time.Sleep(time.Second * 10)
+		} else {
+			keep_waiting_attach = false
+		}
+		//fmt.Println(avresp)
+	}
+	return save_volume_id
+}
+
+func ssh_cmd(instance_ip string, command string) (success bool) {
+	ssh := &easyssh.MakeConfig{
+		User:   "root",
+		Server: instance_ip,
+		// Optional key or Password without either we try to contact your agent SOCKET
+		Key:  "/.ssh/newcarl.pem",
+		Port: "22",
+	}
+	// Call Run method with command you want to run on remote server.
+	_, err := ssh.Run(command)
+
+	// Handle errors
+	if err != nil {
+		fmt.Println("Can't run remote command: " + err.Error())
+		//fmt.Println(response)
+		return false
+	} else {
+		//fmt.Println(response)
+		return true
+	}
+}
+
+func ssh_cp(instance_ip string, file string) {
+	ssh := &easyssh.MakeConfig{
+		User:   "root",
+		Server: instance_ip,
+		// Optional key or Password without either we try to contact your agent SOCKET
+		Key:  "/.ssh/newcarl.pem",
+		Port: "22",
+	}
+	// Call Scp method with file you want to upload to remote server.
+	err := ssh.Scp(file)
+
+	// Handle errors
+	if err != nil {
+		panic("Can't run remote command: " + err.Error())
+	} else {
+		fmt.Println("success uploading " + file)
+	}
+}
+
+func detach_volume_from_instance(client *ec2.EC2, volume_id *string, instance_id *string) (success bool) {
+	params := &ec2.DetachVolumeInput{
+		VolumeId:   volume_id, // Required
+		InstanceId: instance_id }
+	_, err := client.DetachVolume(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	//fmt.Println(resp)
+	return true
+}
+
+func wait_for_volume_available(client *ec2.EC2, volume_id *string) {
+	keep_waiting := true
+	for keep_waiting {
+		params := &ec2.DescribeVolumesInput{
+			VolumeIds: []*string{
+				volume_id,
+			},
+		}
+		resp, err := client.DescribeVolumes(params)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		//fmt.Println(resp)
+		if *resp.Volumes[0].State == ec2.VolumeStateAvailable {
+			fmt.Println("volume is available")
+			break
+		}
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func snapshot_volume(client *ec2.EC2, volume_id *string, snap_title string) (snapshot_id *string) {
+	params := &ec2.CreateSnapshotInput{
+		VolumeId:    volume_id, // Required
+		Description: aws.String(snap_title),
+	}
+	resp, err := client.CreateSnapshot(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	//fmt.Println(resp)
+	return resp.SnapshotId
+}
+
+func wait_snapshot(client *ec2.EC2, snapshot_id *string) {
+	params := &ec2.DescribeSnapshotsInput{
+		SnapshotIds: []*string{ snapshot_id }}
+	err := client.WaitUntilSnapshotCompleted(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func create_image(client *ec2.EC2, snapshot_id *string, title string) (image_id *string) {
+	params := &ec2.RegisterImageInput{
+		Name:         aws.String(title), // Required
+		BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+			{ // Required
+				DeviceName: aws.String("/dev/sda1"),
+				Ebs: &ec2.EbsBlockDevice{
+					DeleteOnTermination: aws.Bool(true),
+					SnapshotId:          snapshot_id,
+					VolumeSize:          aws.Int64(10),
+					VolumeType:          aws.String("gp2"),
+				},
+			},
+			// More values...
+		},
+		Architecture:				aws.String(ec2.ArchitectureValuesX8664),
+		Description:        aws.String(title),
+		RootDeviceName:     aws.String("/dev/sda1"),
+		//SriovNetSupport:    aws.String("String"),
+		VirtualizationType: aws.String("hvm"),
+	}
+	resp, err := client.RegisterImage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	//fmt.Println(resp)
+	return resp.ImageId
+}
+
+func cleanup(client *ec2.EC2, volume_id *string) {
+	// DELETE VOLUME
+	params := &ec2.DeleteVolumeInput{
+		VolumeId: volume_id,
+	}
+	_, err := client.DeleteVolume(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func harvest_software_versions(client *ec2.EC2, instance_ip *string) {
+	ssh_cmd(*instance_ip, "pacman -Q linux-ec2 2> /dev/null")
+	ssh_cmd(*instance_ip, "pacman -Q kubernetes 2> /dev/null")
+	ssh_cmd(*instance_ip, "pacman -Q etcd 2> /dev/null")
+	ssh_cmd(*instance_ip, "pacman -Q rkt 2> /dev/null")
+	ssh_cmd(*instance_ip, "pacman -Q docker 2> /dev/null")
+}
+
+func main() {
+	// Flags
+	var interactive_flag = flag.Bool("interactive", false, "pause for user interaction")
+	var image_basename = flag.String("image-name", "blackbird", "the basename of the image to create")
+	var payload_script_name = flag.String("payload-script", "./chrootalpine.sh", "name of build script in cwd")
+	var use_existing_builder = flag.Bool("use-existing-builder", true, "use the existing build instance")
+	flag.Parse()
+
+	// Main Logic
+  t := time.Now()
+  timestamp := t.Format("2006-01-02-150405")
+	image_title := *image_basename + timestamp
+	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("us-west-2")})
+	// Check for existing builder
+	spot_req_id := aws.String("")
+	if *use_existing_builder {
+		dat, err := ioutil.ReadFile(".hello.state")
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Print(string(dat))
+		fmt.Println("Using existing builder from " + string(dat))
+		spot_req_id = aws.String(string(dat))
+	} else {
+		fmt.Println("Requesting spot instance...")
+		spot_req_id = req_spot(svc, "ami-11718071", "0.018")
+		write_spot_state(spot_req_id)
+	}
+	//spot_req_id := aws.String("sir-03hfzd70")
+	fmt.Println("Waiting for spot instance to become active...")
+	instance_id := wait_for_spot_req_active(svc, spot_req_id)
+  // get it's IP address
+	fmt.Println("Getting instance ip address...")
+	instance_ip := get_instance_ip(svc, instance_id)
+	fmt.Println("Instance IP address: " + *instance_ip)
+	// create and attach volume
+	fmt.Println("Creating and attaching new volume...")
+	volume_id := create_and_attach_volume(svc, instance_id)
+	fmt.Println("Waiting for ssh connect...")
+	// start sshing there
+	for {
+	  if ssh_cmd(*instance_ip, "/bin/true") { break }
+		time.Sleep(time.Second * 5)
+	}
+	// SCP the payload install file
+	ssh_cp(*instance_ip, *payload_script_name)
+	//panic("ok ready to DEVELOP")
+	ssh_cmd(*instance_ip, "chmod +x " + *payload_script_name)
+	// ssh there and run the payload
+	fmt.Println("Running" + *payload_script_name)
+  if ssh_cmd(*instance_ip, "sudo " + *payload_script_name + " /dev/xvdx 2> install-stderr.log") {
+		fmt.Println("script success!")
+		//ssh_cmd(*instance_ip, "sudo systemctl halt")// poweroff
+	} else {
+		fmt.Println("fail!")
+  	ssh_cmd(*instance_ip, "cat install-stderr.log")
+		//fmt.Println("umounting /mnt")
+		//ssh_cmd(*instance_ip, "sudo umount /mnt")
+		fmt.Println("Builder IP: " + *instance_ip)
+		fmt.Println("Image build payload failed!  Aborting in-flight for inspection.")
+		fail_reader := bufio.NewReader(os.Stdin)
+	  fmt.Print("If you want to fix this manually. Press ENTER to continue the build (CTRL-C to abort):")
+	  fail_reader.ReadString('\n')
+	}
+	// Pause for user inspection
+	if *interactive_flag {
+		reader := bufio.NewReader(os.Stdin)
+	  fmt.Print("We're done in the chroot. Press ENTER to continue:")
+	  reader.ReadString('\n')
+	}
+	// detach the volume
+	fmt.Println("Detaching volume...")
+	if detach_volume_from_instance(svc, volume_id, instance_id) {
+		fmt.Println("volume" + *volume_id + "detached")
+	} else {
+		panic("Fatal Error: failed to detach volume")
+	}
+	// wait for volume to be "available"
+	wait_for_volume_available(svc, volume_id)
+	// TODO: terminate builder instance?
+	//# snapshot volume
+	fmt.Println("Snapshot volume...")
+	snapshot_id := snapshot_volume(svc, volume_id, image_title)
+	//# wait for snapshot to be "completed"
+	fmt.Println("Waiting for snapshot to be completed...")
+	wait_snapshot(svc, snapshot_id)
+	//# create AMI from snapshot
+	ami_id := create_image(svc, snapshot_id, image_title)
+	fmt.Println("AMI finished build: " + *ami_id)
+  // cleanup un-necessary artifacts
+	fmt.Println("Cleanup temporary volume...")
+	cleanup(svc, volume_id)
+	//# launch AMI
+	fmt.Println("Launching test instance with this new AMI...")
+	test_spot_req_id := req_spot(svc, *ami_id, "0.018")
+	test_instance_id := wait_for_spot_req_active(svc, test_spot_req_id)
+	test_instance_ip := get_instance_ip(svc, test_instance_id)
+	fmt.Println("test instance public ip: " + *test_instance_ip)
+	//harvest_software_versions(svc, test_instance_ip)
+}
---
+++ chrootalpine.sh
@@ -0,0 +1,123 @@
+#!/bin/bash -ex
+
+dev=/dev/xvdx
+dev_dir=/bootstrap
+final_stage=/mnt
+mirror=http://dl-4.alpinelinux.org/alpine
+branch=v3.3
+
+pacman -Sy --noconfirm parted arch-install-scripts syslinux
+
+(
+	mkdir -p $dev_dir
+	cd $dev_dir
+
+	if ! test -f apk-tools-static-2.6.5-r1.apk; then
+		curl -L $mirror/latest-stable/main/x86_64/apk-tools-static-2.6.5-r1.apk -o apk-tools-static-2.6.5-r1.apk
+		tar -xzvf apk-tools-static-2.6.5-r1.apk
+	fi
+
+  $dev_dir/sbin/apk.static -X ${mirror}/latest-stable/main -U --allow-untrusted --root ${dev_dir} --initdb add alpine-base
+
+	mkdir -p etc/apk
+	mkdir -p proc sys dev tmp
+	touch etc/resolv.conf
+
+	echo "${mirror}/${branch}/main" > etc/apk/repositories
+	echo "${mirror}/${branch}/community" >> etc/apk/repositories
+	# echo "${mirror}/${branch}/testing" >> etc/apk/repositories
+
+	arch-chroot $dev_dir /sbin/apk.static update
+	arch-chroot $dev_dir /sbin/apk.static add bash bkeymaps curl
+
+	cat <<EOF> $dev_dir/alpine-answers
+KEYMAPOPTS="us us"
+HOSTNAMEOPTS="-n alpine-ec2"
+INTERFACESOPTS="auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+	hostname alpine-ec2
+"
+DNSOPTS="-d example.com 8.8.8.8"
+TIMEZONEOPTS="-z UTC"
+PROXYOPTS="none"
+APKREPOSOPTS="-r"
+SSHDOPTS="-c openssh"
+NTPOPTS="-c busybox"
+DISKOPTS="-m sys $dev"
+NOCOMMIT="no"
+ERASE_DISKS="-m sys $dev"
+EOF
+
+	cat <<EOF> /bootstrap/fixpath-runsetup.sh
+#!/bin/sh
+export PATH=${PATH}:/bin:/sbin
+/sbin/setup-alpine -f /alpine-answers
+EOF
+	chmod +x /bootstrap/fixpath-runsetup.sh
+
+	# fix setup-disk for non uname detect
+	sed -i 's%kver=$(uname -r)%kver=grsec%' /bootstrap/sbin/setup-disk
+	# manually disable prompting TODO: figure out how to do this with ERASE_DISKS answer
+	sed -i 's%local erasedisks="$@"%return 0%' /bootstrap/sbin/setup-disk
+  # Then run arch-chroot /setup /sbin/setup-alpine
+	arch-chroot $dev_dir /fixpath-runsetup.sh
+
+	#
+	## finalize setup
+	#
+	umount -l /bootstrap/proc
+	umount -l /bootstrap/dev
+	umount -l /bootstrap/sys
+	mount /dev/xvdf3 $final_stage
+
+	cat <<EAR> $final_stage/etc/local.d/00_populate_ssh_authorized_keys.start
+#!/bin/bash -e
+
+if [ ! -d /root/.ssh ] ; then
+        mkdir -p /root/.ssh
+        chmod 700 /root/.ssh
+fi
+
+function curl_retry {
+        OUTPUT=
+        attempts=0
+        while [ -z "\$OUTPUT" ]; do
+                [ \$attempts -ge 30 ] && return 1
+                OUTPUT="\$(curl -s \$1)"
+                	[ ! -z "\$OUTPUT" ] && break
+                sleep 10
+                let attempts+=1
+        done
+        echo "\$OUTPUT"
+        return 0
+}
+
+KEY=\$(curl_retry http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key)
+if [ \$? -eq 0 ]; then
+        if ! grep -q "^\${KEY}\$" /root/.ssh/authorized_keys 2>/dev/null; then
+                echo "\${KEY}" >> /root/.ssh/authorized_keys
+                chmod 0644 /root/.ssh/authorized_keys
+        fi
+fi
+EAR
+	chmod +x $final_stage/etc/local.d/00_populate_ssh_authorized_keys.start
+	arch-chroot $final_stage /sbin/rc-update add local default
+
+	# setup sshd config
+	cat << EOF > $final_stage/etc/ssh/sshd_config
+Port 22
+PubkeyAuthentication yes
+AuthorizedKeysFile	.ssh/authorized_keys
+PasswordAuthentication no
+PermitRootLogin without-password
+ChallengeResponseAuthentication no
+Subsystem	sftp	/usr/lib/ssh/sftp-server
+EOF
+	# TODO: re-generate host keys
+
+	umount $final_stage
+
+)
